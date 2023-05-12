1e4bc41712f8da35931e4f329f73b56ef32ecc3c
rysrecn <rysrecn@wwu.edu>
2023-05-11 18:26:57 -0700

    create csv file for us states

--- mine.py
+++ mine.py
@@ -1,7 +1,14 @@
 
+import pandas
 
 def main():
-    print("test")
+    data = pandas.read_csv("https://storage.googleapis.com/covid19-open-data/v3/epidemiology.csv")
+    headers = list(data.columns.values)
+    print(headers)
+    data = data[data["location_key"].astype("str").str.startswith('US')]
+    data = data[data["location_key"].astype("str").str.len() == 5]
+    data.to_csv("US.csv")
+    # pandas.DataFrame(data.location_key.unique()).to_csv("unique.csv")
 
 
 if __name__ == "__main__":
